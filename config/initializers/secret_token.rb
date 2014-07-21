20a4293af9645a302e4a82bd011a8563ac87c4af
Yu Zhai <jacky.zhai@gmail.com>
2014-07-21 15:19:51 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Reserveme::Application.config.secret_token = '972bfc8224417618b573ca9fb29e1ce264371aecdbfac79c4d82f7bfd29be25c6051136b0173e144ca930d6cbe37a1b18e18d09b6eaea3e09ef37e9b58c57253'
